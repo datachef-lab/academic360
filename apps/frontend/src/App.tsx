ca7eb9bbb74ed34e1ccb4d9f23cd01bc1b3b4b63
desaiHarsh08 <desaiharsh183@gmail.com>
2025-01-01 18:38:38 +0530

    Initial Router setup

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,14 +1,14 @@
-import MyComponent from "@/components/MyComponent";
-import { Button } from "@/components/ui/button";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home, Login, Root } from "@/pages";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Root /> },
+  { path: "/login", element: <Login /> },
+  { path: "/home", element: <Home /> },
+]);
 
 const App = () => {
-  return (
-    <div className="text-center">
-      App
-      <MyComponent />
-      <Button>Click me</Button>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ apps/frontend/src/pages/index.ts
@@ -0,0 +1,20 @@
+import AddStudent from "./AddStudent";
+import GetReports from "./GetReports";
+import Home from "./Home";
+import Login from "./Login";
+import Root from "./Root";
+import SearchStudent from "./SearchStudent";
+import Settings from "./Settings";
+import UserProfile from "./UserProfile";
+
+
+export {
+    Root,
+    Login,
+    Home,
+    AddStudent,
+    GetReports,
+    SearchStudent,
+    Settings,
+    UserProfile
+}
