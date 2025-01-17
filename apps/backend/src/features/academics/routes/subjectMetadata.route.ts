9cf1b617ff6468474009a28e7a6f05e8eae85c96
desaiHarsh08 <desaiharsh183@gmail.com>
2025-01-17 17:57:07 +0530

    fixed the types for fetching data by id, update, and delete apis.

--- apps/backend/src/features/academics/routes/subjectMetadata.route.ts
+++ apps/backend/src/features/academics/routes/subjectMetadata.route.ts
@@ -5,12 +5,31 @@ import { createSubjectMetadata, deleteSubjectMetadata, getAllSubjectMetadatas, g
 const router = express.Router();
 
 router.post("/", createSubjectMetadata);
-router.get("/", getAllSubjectMetadatas);
-router.get("/:id",getSubjectMetadataById);
-router.get("/stream/:streamId", getSubjectMetadataByStreamId);
-router.get("/semester/:semester", getSubjectMetadataBySemester);
-router.get("/stream/:streamId/semester/:semester", getSubjectMetadataByStreamIdAndSemester);
+
+router.get("/", getAllSubjectMetadatas, );
+
+router.get("/query", (req, res, next) => {
+    const { id, streamId, semester } = req.query;
+    console.log(id, streamId, semester);
+    if (semester && streamId) {
+        getSubjectMetadataByStreamIdAndSemester(req, res, next);
+    }
+    else if (id) {
+        getSubjectMetadataById(req, res, next);
+    }
+    else if (streamId) {
+        getSubjectMetadataByStreamId(req, res, next)
+    }
+    else if (semester) {
+        getSubjectMetadataBySemester(req, res, next)
+    }
+    else {
+        getAllSubjectMetadatas(req, res, next);
+    }
+})
+
 router.put("/:id", updateSubjectMetadata);
+
 router.delete("/:id", deleteSubjectMetadata);
 
 export default router;
