d0d658796e2ee6711841a54ce6136c91a04df639
desaiHarsh08 <desaiharsh183@gmail.com>
2025-02-18 12:26:13 +0530

    added routes and controller for marksheet-apis

--- apps/backend/src/features/academics/routes/marksheet.route.ts
+++ apps/backend/src/features/academics/routes/marksheet.route.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { verifyJWT } from "@/middlewares/verifyJWT.js";
-import { addMultipleMarksheet } from "../controllers/marksheet.controller.js";
+import { addMultipleMarksheet, createMarksheet, getMarksheetById, updatedMarksheet } from "../controllers/marksheet.controller.js";
 import { uploadExcelMiddleware } from "@/middlewares/uploadMiddleware.middleware.js";
 import { deleteTempFile } from "@/middlewares/deleteTempFile.middleware.js";
 
@@ -8,6 +8,12 @@ const router = express.Router();
 
 router.use(verifyJWT);
 
-router.get("/upload", uploadExcelMiddleware, addMultipleMarksheet, deleteTempFile)
+router.get("/upload", uploadExcelMiddleware, addMultipleMarksheet, deleteTempFile);
+
+router.post("/", createMarksheet);
+
+router.get("/:id", getMarksheetById);
+
+router.put("/:id", updatedMarksheet);
 
 export default router;
