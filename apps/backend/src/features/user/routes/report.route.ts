c0abf8c456bbcee251f4bb5829ebce619a7ee6ef
desaiHarsh08 <desaiharsh183@gmail.com>
2025-04-06 11:41:32 +0530

    fixed version conflict for switch

--- apps/backend/src/features/user/routes/report.route.ts
+++ apps/backend/src/features/user/routes/report.route.ts
@@ -1,12 +1,12 @@
 import { verifyJWT } from "@/middlewares/verifyJWT.js";
 import express from "express";
-import {  getAllReports } from "../controllers/report.controller";
+import { getAllReports } from "../controllers/report.controller";
 
 const router = express.Router();
 
 router.use(verifyJWT);
 // router.get("/:id",getReportId);
-router.get("/query", getAllReports); 
+router.get("/query", getAllReports);
 
 
 export default router;
