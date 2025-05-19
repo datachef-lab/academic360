de051ee78b546e7e821bfaa22dd2117583391bd9
desaiHarsh08 <desaiharsh183@gmail.com>
2025-05-19 14:13:05 +0530

    created api

--- apps/backend/src/features/academics/routes/marksheet.route.ts
+++ apps/backend/src/features/academics/routes/marksheet.route.ts
@@ -2,25 +2,28 @@ import express from "express";
 
 // import { verifyJWT } from "@/middlewares/verifyJWT.js";
 
-import { addMultipleMarksheet, createMarksheet, getAllMarksheets, getMarksheetById, getMarksheetsLogs, updatedMarksheet } from "../controllers/marksheet.controller.js";
+import { addMultipleMarksheet, createMarksheet, getAllMarksheets, getMarksheetById, getMarksheetsLogs, getMarksheetSummary, updatedMarksheet } from "../controllers/marksheet.controller.js";
 import { uploadExcelMiddleware } from "@/middlewares/uploadMiddleware.middleware.js";
 import { deleteTempFile } from "@/middlewares/deleteTempFile.middleware.js";
 import { verifyJWT } from "@/middlewares/verifyJWT.js";
 
 const router = express.Router();
 
-router.use(verifyJWT);
+// router.use(verifyJWT);
 
-router.post("/upload", uploadExcelMiddleware, addMultipleMarksheet, deleteTempFile);
+router.post("/upload", verifyJWT, uploadExcelMiddleware, addMultipleMarksheet, deleteTempFile);
 
-router.post("/", createMarksheet);
+router.post("/", verifyJWT, createMarksheet);
 
-router.get("/logs", getMarksheetsLogs);
-router.get("/query", getAllMarksheets);
+router.get("/logs", verifyJWT, getMarksheetsLogs);
 
-router.get("/:id", getMarksheetById);
+router.get("/summary", getMarksheetSummary);
+
+router.get("/query", verifyJWT, getAllMarksheets);
+
+router.get("/:id", verifyJWT, getMarksheetById);
 
 
-router.put("/:id", updatedMarksheet);
+router.put("/:id", verifyJWT, updatedMarksheet);
 
 export default router;
