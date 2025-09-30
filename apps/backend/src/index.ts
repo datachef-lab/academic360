d3da27deb48cff295ce8df7762bd50f31a4dfc73
desaiHarsh08 <desaiharsh183@gmail.com>
2025-09-30 13:02:44 +0530

    fixed staging cd v9

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -50,7 +50,7 @@ function checkRequiredEnvs() {
   // checkRequiredEnvs(); // WILL BE NEED TO UNCOMMENT
   try {
     await connectToDatabase();
-    console.log("NODE_ENV: updated v8", process.env.NODE_ENV);
+    console.log("NODE_ENV: updated v9", process.env.NODE_ENV);
     if (
       process.env.NODE_ENV === "production" ||
       process.env.NODE_ENV === "development"
