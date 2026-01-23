d776ba3d77e0960e9a6e789c04384615b9737fe3
RounitSingh <rounitsingh2002@gmail.com>
2026-01-23 10:46:37 +0530

    removed the green indicator

--- apps/main-console/src/components/globals/ActiveUsersAvatars.tsx
+++ apps/main-console/src/components/globals/ActiveUsersAvatars.tsx
@@ -161,7 +161,6 @@ export function ActiveUsersAvatars() {
               ))}
 
               {/* Online indicator */}
-              <div className="h-3 w-3 rounded-full bg-green-500 ring-2 ring-white" />
             </div>
           )}
           {studentsOnlineCount > 0 && (
