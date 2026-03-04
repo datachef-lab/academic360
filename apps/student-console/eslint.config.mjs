ac95ae5c4779c57ffe5f03ecd826593dfe47673d
desaiHarsh08 <desaiharsh183@gmail.com>
2026-03-04 14:36:36 +0530

    Configure the eslint

--- apps/student-console/eslint.config.mjs
+++ apps/student-console/eslint.config.mjs
@@ -1,16 +1,22 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+import {nextJsConfig} from "@repo/eslint-config/next-js"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+//   ...nextJsConfig,
+// ];
+
+// export default eslintConfig;
+
+export default [
+  ...nextJsConfig
 ];
-
-export default eslintConfig;
--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -1,11 +1,8 @@
-import js from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
-import tseslint from "typescript-eslint";
-import pluginReactHooks from "eslint-plugin-react-hooks";
+import baseConfig from "./base.js";
 import pluginReact from "eslint-plugin-react";
-import globals from "globals";
+import pluginReactHooks from "eslint-plugin-react-hooks";
 import pluginNext from "@next/eslint-plugin-next";
-import { config as baseConfig } from "./base.js";
+import globals from "globals";
 
 /**
  * A custom ESLint configuration for libraries that use Next.js.
@@ -14,35 +11,31 @@ import { config as baseConfig } from "./base.js";
  * */
 export const nextJsConfig = [
   ...baseConfig,
-  js.configs.recommended,
-  eslintConfigPrettier,
-  ...tseslint.configs.recommended,
+
+  // React flat config
+  pluginReact.configs.flat.recommended,
+
   {
-    ...pluginReact.configs.flat.recommended,
     languageOptions: {
-      ...pluginReact.configs.flat.recommended.languageOptions,
       globals: {
+        ...globals.browser,
         ...globals.serviceworker,
       },
     },
   },
+
   {
     plugins: {
       "@next/next": pluginNext,
+      "react-hooks": pluginReactHooks,
+    },
+    settings: {
+      react: { version: "detect" },
     },
     rules: {
       ...pluginNext.configs.recommended.rules,
       ...pluginNext.configs["core-web-vitals"].rules,
-    },
-  },
-  {
-    plugins: {
-      "react-hooks": pluginReactHooks,
-    },
-    settings: { react: { version: "detect" } },
-    rules: {
       ...pluginReactHooks.configs.recommended.rules,
-      // React scope no longer necessary with new JSX transform.
       "react/react-in-jsx-scope": "off",
     },
   },
