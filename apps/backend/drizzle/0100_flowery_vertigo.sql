1f4ca2a1f1a7d7897af17e5946de7381a4f30b27
desaiHarsh08 <desaiharsh183@gmail.com>
2026-03-20 16:41:12 +0530

    feat(backend): add default administration data

---
+++ apps/backend/drizzle/0100_flowery_vertigo.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "app_modules" ADD COLUMN "application" "academic360_application_domain_type" NOT NULL;--> statement-breakpoint
+ALTER TABLE "user_statuses_master" ADD COLUMN "bg_color" varchar(255);
